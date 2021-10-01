3abf59950aa6490648d03aca7918031910da3b4d
unknown <sagarsamanta390@gmail.com>
2021-10-01 18:52:33 +0530

    set enviourment

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 const authentication = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = jwt.verify(token, "thisismynewcourse");
+    const decoded = jwt.verify(token,process.env.JWT_SECREAT_KEY);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token, 
